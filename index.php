77903bc9940223fdb10782317c6b378155187f9d
Nena Ani <nenaani@MacBook-Pro-van-Nena.local>
2018-03-19 09:51:07 +0100

    start zeeslag

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+    CreateTable();
+
+    function CreateTable(){
+        echo "<table>";
+
+        for($posx = 0; $posx < 10; $posx++){
+                echo '<tr style="background-color:#000080">';
+                for($posy = 0; $posy < 10; $posy++){
+//                  echo colors($posx, $posy);
+
+                } echo "</tr>";
+        } 
+    }   echo "</table>";
+    
+    
+    
+    
+?>
